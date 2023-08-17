8b78ed68d4d27adfd2a0a353363bfd51ca24b50a
ransayada <ran.sayada@gmail.com>
2023-08-17 15:24:11 +0300

    add clerk auth for client

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
 const DashboardPage = () => {
-  return <div>DashboardPage (Protected)</div>;
+  return (
+    <div>
+      <p>DashboardPage (Protected)</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default DashboardPage;
